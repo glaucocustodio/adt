3732bb05ff784528b1724fe90fcd23edf59a348e
Glauco Custódio <glauco.custodio@gmail.com>
2016-03-22 21:20:18 -0300

    Seeds content back again. Add sanity where condition.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     MakeService.new.result
-    @makes = Make.all.order('name ASC')
+    @makes = Make.where.not(webmotors_id: nil).order('name ASC')
   end
 end
